8ccf0bb62733c4f17fac4d9b873a0596d9c9f37b
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-11-30 22:49:07 -0500

    Add jsx to create conditional button components

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,26 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   
+   // Logic to check which class was passed to the button via props
+
+   let buttonClass = "button";
+
+   if (props.confirm) {
+     buttonClass += " button--confirm";
+   } 
+   
+   if (props.danger) {
+      buttonClass += " button--danger";
+   }
+
+   return (
+      <button 
+         className={buttonClass}
+         onClick={props.onClick}
+         disabled={props.disabled}
+      >
+         {props.children}
+      </button>
+   ); 
 }
