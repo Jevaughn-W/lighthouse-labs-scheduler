8139abff7af3892479e2cd06931d976660e5283b
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-12-04 14:25:09 -0500

    Add component to render list of interviewers using interviewerListItem

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+import "./InterviewerList.scss";
+
+export default function InterviewerList(props) {
+  const interviewers = props.interviewers.map((interviewer) => {
+    return (<InterviewerListItem
+      key={interviewer.id}
+      avatar={interviewer.avatar}
+      name={interviewer.name}
+      id={interviewer.id}
+      setInterviewer={props.setInterviewer}
+      selected={props.interviewer === interviewer.id}
+    />
+    );
+  });
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {interviewers}
+      </ul>
+    </section>
+  );
+}
