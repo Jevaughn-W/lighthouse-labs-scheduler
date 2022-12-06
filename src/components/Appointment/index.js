d750056db06955ef95a3c7de45ee7755f2d055b1
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-12-06 14:30:36 -0500

    Add header, show and empty components to application component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 import "./styles.scss";
 
 export default function Appointment(props) {
-  return (<article className="appointment"></article>)
+
+  return (
+  <article className="appointment">
+    <Header time={props.time}/>
+    {props.interview ? <Show interviewer={props.interview.interviewer.name} student={props.interview.student}/> : <Empty/>}
+  </article>)
 }
