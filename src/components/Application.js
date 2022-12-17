2bff4eaebfb611cf653a56aed1619113eaa40324
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-12-17 17:55:26 -0500

    Update function to select existing appointment, with interview and id the update appointments state with the new appointment object

--- src/components/Application.js
+++ src/components/Application.js
@@ -9,25 +9,21 @@ import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "help
 
 export default function Application(props) {
   
-  function bookInterview(id, interview) {
-    console.log(id, interview);
-  }
-
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {},
     interviewers: {},
   })
-
+  
   let dailyAppointments = [];
   let dailyInterviewers = [];
-
+  
   // Functions to handle state change
   const setDay = day => setState(prev =>({ ...prev, day }));
-
+  
   // UseEffect to make API call to get days data on render
-
+  
   useEffect(() => {
     
     const routes = {
@@ -35,7 +31,7 @@ export default function Application(props) {
       getAppointments: "http://localhost:8001/api/appointments",
       getInterviews: "http://localhost:8001/api/interviewers"
     };
-
+    
     // Updating states with data obtained from axios requests
     Promise.all([
       axios.get(routes.getDays),
@@ -45,15 +41,30 @@ export default function Application(props) {
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
     });
   }, [])
-
+  
   // Getting daily appointments
   dailyAppointments = getAppointmentsForDay(state, state.day);
   dailyInterviewers = getInterviewersForDay(state, state.day);
+  
+  // Function to add in new interview for the day
+  function bookInterview(id, interview) {
 
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    setState(prev => ({...prev, appointments}));
+  }
   // Function to render list of appointments after converting object to array
   const appointmentList = dailyAppointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
-
+    
     return (
       <Appointment
         key={appointment.id}
