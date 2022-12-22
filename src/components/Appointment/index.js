f1859975e8208f2b84a3aaf8ff1a34f2a185d33a
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-12-21 21:58:59 -0500

    Update formatting of return

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -58,69 +58,70 @@ export default function Appointment(props) {
   }
   
   return (
-  <article className="appointment">
-    <Header time={props.time}/>
-
-    {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-    {mode === SHOW && (
-      <Show
-        student={props.interview.student}
-        interviewer={props.interview.interviewer.name}
-        onDelete={deleteAppointment}
-        onEdit={() => transition(EDIT)}
-      />
-    )}
-
-    {mode === CREATE && (
-      <Form
+    <article className="appointment">
+      <Header time={props.time}/>
+    
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+          onDelete={deleteAppointment}
+          onEdit={() => transition(EDIT)}
+        />
+      )}
+  
+      {mode === CREATE && (
+        <Form
+          interviewers={props.interviewers}
+          onSave={save}
+          onCancel={back}
+          appointmentId={props.id}
+        />
+      )}
+  
+      {mode === SAVING && (
+        <Status
+          message="Saving"
+        />
+      )}
+      {mode === CONFIRM && (
+        <Confirm
+          onConfirm={deleteConfirmed}
+          onCancel={back}
+          message="Are you sure you would like to delete?"
+        />
+      )}
+      {mode === DELETING && (
+        <Status
+        message="Deleting"
+        />
+      )}
+  
+      {mode === EDIT && (
+        <Form
         interviewers={props.interviewers}
-        onSave={save}
-        onCancel={back}
-        appointmentId={props.id}
-      />
-    )}
-
-    {mode === SAVING && (
-      <Status
-        message="Saving"
-      />
-    )}
-    {mode === CONFIRM && (
-      <Confirm
-        onConfirm={deleteConfirmed}
-        onCancel={back}
-        message="Are you sure you would like to delete?"
-      />
-    )}
-    {mode === DELETING && (
-      <Status
-      message="Deleting"
-      />
-    )}
-
-    {mode === EDIT && (
-      <Form
-      interviewers={props.interviewers}
-        student={props.interview.student}
-        interviewer={props.interview.interviewer.id}
-        onSave={save}
-        onCancel={back}
-        appointmentId={props.id}
-      />
-    )}
-
-    {mode === ERROR_SAVE && (
-      <Error
-      message="Unable to save appointment"  
-      onClose={back}
-      />
-    )}
-    {mode === ERROR_DELETE && (
-      <Error
-        message="Unable to delete appointment"
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.id}
+          onSave={save}
+          onCancel={back}
+          appointmentId={props.id}
+        />
+      )}
+  
+      {mode === ERROR_SAVE && (
+        <Error
+        message="Unable to save appointment"  
         onClose={back}
-      />
-    )}
-
-  </article>)
+        />
+      )}
+      {mode === ERROR_DELETE && (
+        <Error
+          message="Unable to delete appointment"
+          onClose={back}
+        />
+      )}
+  
+    </article>
+  )
 }
