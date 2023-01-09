b81747b2d909f2d54b666ae2f4c1b8b97b045b16
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-01-08 23:38:45 -0500

    Add validation functions and error message if student or interviewer is null

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -17,6 +17,22 @@ export default function Form(props)  {
     props.onCancel();
   }
 
+  const [error, setError] = useState("");
+
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+  
+    props.onSave(student, interviewer);
+  }
+
   return(
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -28,8 +44,10 @@ export default function Form(props)  {
             type="text"
             placeholder="Enter Student Name"
             onChange={(event) => {setStudent(event.target.value)}}
+            data-testid="student-name-input"
             />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList 
           value={interviewer}
           interviewers={props.interviewers}
@@ -39,7 +57,7 @@ export default function Form(props)  {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel} >Cancel</Button>
-          <Button confirm onClick={() => props.onSave(student, interviewer)} >Save</Button>
+          <Button confirm onClick={() => validate()} >Save</Button>
         </section>
       </section>
   </main>
