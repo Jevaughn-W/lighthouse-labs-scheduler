de06fb524660c930c1802e1b2a4b864e17ce00e4
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-01-16 06:40:02 -0700

    Add comments to clearly identify sections and format HTML

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -2,15 +2,15 @@ import React from "react";
 
 export default function Show(props) {
   return(
-    <main className="appointment__card appointment__card--show">
-      <section className="appointment__card-left">
+    <main className="appointment__card appointment__card--show"> {/* Main appointment card */}
+      <section className="appointment__card-left"> {/* Shows details about the student and inteview if in Show mode */}
         <h2 className="text--regular">{props.student}</h2>
         <section className="interviewer">
           <h4 className="text--light">Interviewer</h4>
           <h3 className="text--regular">{props.interviewer}</h3>
         </section>
       </section>
-      <section className="appointment__card-right">
+      <section className="appointment__card-right"> {/* Section containing edit and delete buttons which appear on hover */}
         <section className="appointment__actions">
           <img
             className="appointment__actions-button"
@@ -24,8 +24,8 @@ export default function Show(props) {
             alt="Delete"
             onClick={props.onDelete}
           />
-      </section>
-    </section>
-  </main>
+        </section>
+     </section>
+    </main>
   )
 }
